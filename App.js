778bb21a799fa9153c65af8e5230be638961136a
jorgechvz <jorgechvzpon@gmail.com>
2024-03-14 00:51:42 -0500

    Refactor styling in App.js

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  titleStiles = StyleSheet.create({
+    container: {
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: 40,
+      fontWeight: "bold",
+      marginBottom: 20,
+    },
+  });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={titleStiles.container}
+      >
+        E-commerce App
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: "center",
+        }}
+      >
+        This is a e-commerce app with react native
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
