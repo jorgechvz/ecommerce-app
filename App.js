0cc44e2d3a05f254e1f0c892a4bcc0d1ab704680
jorgechvz <jorgechvzpon@gmail.com>
2024-03-23 12:30:50 -0500

    Refactor App.js to use SafeAreaProvider and Navigation component

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./components/Navigation";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  titleStiles = StyleSheet.create({
-    container: {
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-      fontSize: 40,
-      fontWeight: "bold",
-      marginBottom: 20,
-    },
-  });
   return (
-    <View style={styles.container}>
-      <Text
-        style={titleStiles.container}
-      >
-        E-commerce App
-      </Text>
-      <Text
-        style={{
-          fontSize: 20,
-          textAlign: "center",
-        }}
-      >
-        This is a e-commerce app with react native
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
