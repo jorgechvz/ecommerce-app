65878b1864ddc53c1ab469c2bcda889f5f9e0644
jorgechvz <jorgechvzpon@gmail.com>
2024-03-23 12:33:44 -0500

    Add SectionHeader component

---
+++ components/SectionHeader.jsx
@@ -0,0 +1,30 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default function SectionHeader({
+  title,
+  navRoute,
+  navigation,
+  isNested,
+}) {
+  const handlePress = () => {
+    if (isNested) {
+      navigation.navigate("SubCategory", navRoute);
+    } else {
+      navigation.navigate(navRoute);
+    }
+  };
+
+  return (
+    <View className="flex-row justify-between items-center mt-5 mx-2 p-2">
+      <Text className="text-[16px]">{title}</Text>
+      <TouchableOpacity
+        className="flex-row justify-between items-center gap-2"
+        onPress={handlePress}
+      >
+        <Text className="text-[16px]">View all</Text>
+        <Icon name="angle-right" size={28} />
+      </TouchableOpacity>
+    </View>
+  );
+}
